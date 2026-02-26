ad7b3fae85c6bd3694aa71207ff5ba7198e97ec8
Lokesh Medharametla <lokesh@flatme.dev>
2026-02-26 13:36:58 +0530

    refactor(routes): remove dead duplicate src/routes/index.ts

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-import { getMeHandler } from "../controllers/userController";
-import { authMiddleware } from "../middleware/authMiddleware";
-
-export const apiRoutes = new Hono();
-
-apiRoutes.use(authMiddleware);
-
-apiRoutes.get("/me", getMeHandler);
